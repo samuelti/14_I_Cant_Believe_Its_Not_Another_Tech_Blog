46208e60553b2e2e22f6d0d7be3bbd0262303042
samuelti <samuelti2442@gmail.com>
2021-08-02 21:10:27 -0700

    added .env file to gitignore

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,4 +1,20 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+
 // get all posts 
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [User, Post]
+    }).then((data) => res.json(data)).catch(err => res.status(500).json(err));
+});
 
 //get a single post
-
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where:{
+            id: req.params.id
+        },
+        include: [Post,User,Comment]
+    }).then((data) => res.json(data))
+    .catch(err => res.status(500).json(err));
+});
